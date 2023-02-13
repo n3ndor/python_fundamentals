5051b783511d547bd882d0d17568972312ebc084
n3ndor <n3ndor@gmail.com>
2023-02-13 08:50:50 -0300

    updated with second solution

--- function_basic_ii.py
+++ function_basic_ii.py
@@ -4,11 +4,14 @@ Create a function that accepts a number as an input. Return a new list that coun
 Example: countdown(5) should return [5,4,3,2,1,0]
 """
 
+
 def countdown(x):
     list = []
     for i in range(x, -1, -1):
         list.append(i)
     return list
+
+
 print(countdown(5))
 
 """"
@@ -17,9 +20,12 @@ Create a function that will receive a list with two numbers. Print the first val
 Example: print_and_return([1,2]) should print 1 and return 2
 """
 
+
 def print_and_return(list):
     print(list[0])
     return list[1]
+
+
 print(print_and_return([3, 7]))
 
 """ 
@@ -28,8 +34,11 @@ Create a function that accepts a list and returns
 the sum of the first value in the list plus the list's length.
 Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 """
+
+
 def first_plus_length(list):
     return print(list[0] + len(list))
+
 
 first_plus_length([10, 2, 3, 4, 5])
 
@@ -42,16 +51,21 @@ have the function return False
 Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 Example: values_greater_than_second([3]) should return False
 """
+
+
 def values_greater_than_second(list):
     if len(list) < 2:
         return print(False)
     else:
         newlist = []
+        # for i in range(0, len(list))
         for i in list:
+            # if list[i] > list[1]
             if i > list[1]:
                 newlist.append(i)
         print(len(newlist))
         return print(newlist)
+
 
 values_greater_than_second([3, 5, 8, 1, 4, 10, 3, 50])
 values_greater_than_second([7])
@@ -66,11 +80,19 @@ Example: length_and_value(4,7) should return [7,7,7,7]
 Example: length_and_value(6,2) should return [2,2,2,2,2,2]
 """
 
+
 def length_and_value(size, value):
     newlist = []
     for i in range(size):
         newlist.append(value)
     return print(newlist)
+
+
+# def length_and_value(size, value):
+#     newlist = []
+#     newlist.append(value)
+#     return print(newlist * size)
+
 
 length_and_value(4, 7)
 length_and_value(6, 2)
